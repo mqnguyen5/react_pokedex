4152438fc2265692c5f3248eee9ebd34caa2efbf
Minh Quan Nguyen <mqnguyen5@myseneca.ca>
2021-08-01 19:21:39 -0400

    add weight, height, abilities, and base stats to PokemonDetails

--- src/components/PokemonDetails.js
+++ src/components/PokemonDetails.js
@@ -13,23 +13,61 @@ export default function PokemonDetails() {
       );
       setPokemon(data);
     }
-
     fetchApi();
   }, [id]);
+
+  const convertPokemonNumber = num => (num / 10).toFixed(1);
+
+  const getPokemonStats = () =>
+    pokemon.stats.map(statData => {
+      let statName = "";
+      switch (statData.stat.name) {
+        case "special-attack":
+          statName = "sp.attack";
+          break;
+        case "special-defense":
+          statName = "sp.defense";
+          break;
+        default:
+          statName = statData.stat.name;
+          break;
+      }
+
+      return (
+        <p key={statData.stat.url}>
+          {statName}: {statData.base_stat}
+        </p>
+      );
+    });
 
   if (!pokemon) return <p>Loading Pokemon's Data...</p>;
 
   return (
     <div>
+      <h1>{pokemon.name.toUpperCase()}</h1>
+
       <p>ID: {pokemon.id}</p>
-      <p>Name: {pokemon.name}</p>
+      <p>Height: {convertPokemonNumber(pokemon.height)}m</p>
+      <p>Weight: {convertPokemonNumber(pokemon.weight)}kg</p>
+      <p>
+        Abilities:{" "}
+        {pokemon.abilities.map(abilityData => (
+          <span key={abilityData.ability.url}>
+            {abilityData.ability.name.toUpperCase()}{" "}
+          </span>
+        ))}
+      </p>
       <p>
         Type:{" "}
         {pokemon.types.map(typeData => (
           <span key={typeData.type.url}>{typeData.type.name} </span>
         ))}
       </p>
+
       <img src={pokemon.sprites.front_default} alt={pokemon.name + " image"} />
+
+      <h3>Base Stats</h3>
+      {getPokemonStats()}
     </div>
   );
 }
